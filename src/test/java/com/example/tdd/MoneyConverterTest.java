a7aef7354ce4892d4766879b13de113716f9af9f
tobiaswes <tobias.west0@gmail.com>
2025-10-28 09:20:40 +0200

    shouldConvertToUsdToEuro
    RÖD!

--- src/test/java/com/example/tdd/MoneyConverterTest.java
+++ src/test/java/com/example/tdd/MoneyConverterTest.java
@@ -1,8 +1,17 @@
 package com.example.tdd;
 
 import org.junit.jupiter.api.Test;
+import java.math.BigDecimal;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MoneyConverterTest {
+
     @Test
-    void testConvert() {}
+    void shouldConvertUsdToEuro() {
+        BigDecimal usd = new BigDecimal("100.00");
+
+        BigDecimal result = MoneyConverter.usdToEuro(usd);
+
+        assertThat(result).isEqualByComparingTo("92.00");
+    }
 }
