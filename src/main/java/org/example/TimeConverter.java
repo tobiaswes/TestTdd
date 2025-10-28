8e763fa25d8bbda84a850fce8211d9e121f34365
tobiaswes <tobias.west0@gmail.com>
2025-10-28 10:52:36 +0200

    Test shouldThrowExceptionForNegativeSeconds GRÖNT!

--- src/main/java/org/example/TimeConverter.java
+++ src/main/java/org/example/TimeConverter.java
@@ -4,7 +4,16 @@ public class TimeConverter {
 
     private static final int SECONDS_PER_MINUTE = 60;
 
-    public static double secondsToMinutes(double seconds) {
-        return seconds / SECONDS_PER_MINUTE;
+    public double secondsToMinutes(double value) {
+
+        validateNonNegative(value, "seconds");
+        return value / SECONDS_PER_MINUTE;
     }
+
+    public static void validateNonNegative(double value, String name) {
+        if (value < 0) {
+            throw new IllegalArgumentException("Value cannot be negative");
+        }
+    }
+
 }
--- src/test/java/com/example/tdd/TimeConverterTest.java
+++ src/test/java/com/example/tdd/TimeConverterTest.java
@@ -1,24 +1,35 @@
 package com.example.tdd;
 
 import org.example.TimeConverter;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class TimeConverterTest {
+
+    private TimeConverter timeConverter;
+
+    @BeforeEach
+    void setUp() {
+        timeConverter = new TimeConverter();
+    }
 
     @Test
     @DisplayName("Should convert 60 seconds to 1 minute")
     void shouldConvert60SecondsTo1Minute() {
-        assertThat(TimeConverter.secondsToMinutes(60))
+        assertThat(timeConverter.secondsToMinutes(60))
                 .isEqualTo(1.0);
     }
 
     @Test
-    void shouldThrowExceptionForNegativeSeconds(){
-        assertThatThrowBy(()-> TimeConverter.secondsToMinutes(-10))
-                .isInstaceOf(IllegalArgumentException.class)
+    @DisplayName("Should throw exception for negative seconds")
+    void shouldThrowExceptionForNegativeSeconds() {
+        assertThatThrownBy(() -> timeConverter.secondsToMinutes(-10))
+                .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("negative");
     }
 }
+
